dad78eb8bd0ec4447d7a4c9789937f13e37980f4
Danieltq04 <daniel.tq04@gmail.com>
2022-02-06 12:40:23 -0300

    primer commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,35 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import ButtonReducer from '../components/ButtonReducer';
+import Movies from '../components/Movies';
+import Post from '../components/Post/Post';
+import { userContext } from '../context/UserContext';
+
+export default function Home() {
+    const [characters,setCharacters] = useState([])
+
+    // Consultando el usuario del contexto ( Global)
+    const {user,setUser} = useContext(userContext)
+
+    useEffect(()=>{
+        async function fetchData(){
+            const res = await fetch("https://thronesapi.com/api/v2/Characters")
+            const data = await res.json()
+
+            setCharacters(data)
+        }
+
+        fetchData()
+    },[]) // No omitir, causa ciclos infinitos
+
+  return <>
+        {console.log(characters)}
+        <p>{user.nombre}</p>
+        <button onClick={()=>{setUser({nombre:"Tzuzul",foto:"Mi foto"})}}>Iniciar sesión</button>
+        <Link to="/home2">Ir a home 2</Link>
+        <ButtonReducer/>
+        <Movies movies={characters}/>
+
+        <Post usuario={user}/>
+  </>;
+}
